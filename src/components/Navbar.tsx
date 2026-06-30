9b189483f036ee9fe372b8cdd48db94136d70396
Stefan Petrovic <stefanpetrovicgta@gmail.com>
2026-07-01 01:25:57 +0200

    Dodat logo hotela u navigaciju

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo2.png";
 import "../styles/Navbar.css";
 
 function Navbar() {
     return (
         <nav>
+              <div className="logo">
+                <img src={logo} alt="Indigo Belgrade"/>
+                <span> Indigo Belgrade</span>
+            </div>
             <div className="links">
                 <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>Početna</NavLink>
                 <NavLink to="/rooms" className={({ isActive }) => isActive ? "active-link" : "" }>  Sobe</NavLink>
